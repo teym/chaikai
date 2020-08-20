15550d9bf3765b17622ebfb3f8b8f97516dd7987
Mike <ccteym@gmail.com>
2020-08-20 18:42:43 +0800

    b_admin

---
+++ b_admin/src/views/dashboard/components/notice.vue
@@ -0,0 +1,43 @@
+<template>
+  <div v-loading="loading" class="notice-container">
+    <p>公告规则</p>
+    <el-row>
+      <el-col v-for="(item, i) in list" :key="i" :span="12">{{ item.title }}</el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+// import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Notice',
+  data() {
+    return {
+      loading: false,
+      list: []
+    }
+  },
+  //   computed: {
+  //     ...mapGetters([
+  //       'roles'
+  //     ])
+  //   },
+  created() {
+    // if (!this.roles.includes('admin')) {
+    //   this.currentRole = 'editorDashboard'
+    // }
+    this.loading = true
+    setTimeout(() => {
+      this.loading = false
+      this.list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((i) => ({
+        title: 'test title' + i,
+        url: 'https://www.baidu.com'
+      }))
+    }, 1000)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ b_admin/src/views/dashboard/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="dashboard-container">
+    <notice />
+    <el-row :gutter="10">
+      <el-col :span="12">
+        <div class="block">
+          <h5>快捷功能</h5>
+          <el-row>
+            <el-col :span="12">创建商品</el-col>
+            <el-col :span="12">发布活动</el-col>
+            <el-col :span="12">认证授权</el-col>
+            <el-col :span="12">申请开票</el-col>
+          </el-row>
+        </div>
+      </el-col>
+      <el-col :span="12">
+        <div class="block">
+          <h5>待办事项</h5>
+          <el-row>
+            <el-col :span="12">创建商品</el-col>
+            <el-col :span="12">创建商品</el-col>
+            <el-col :span="12">创建商品</el-col>
+            <el-col :span="12">创建商品</el-col>
+          </el-row>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import notice from "./components/notice";
+
+export default {
+  name: "Dashboard",
+  components: { notice },
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters(["roles"]),
+  },
+  created() {
+    // if (!this.roles.includes('admin')) {
+    //   this.currentRole = 'editorDashboard'
+    // }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.dashboard-container {
+  padding: 40px 45px 20px 50px;
+  .block {
+    background-color: lightgray;
+    border-radius: 4px;
+  }
+}
+</style>
