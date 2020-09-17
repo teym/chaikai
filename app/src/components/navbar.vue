d91ab95aa6b63c24d0c043c6eeebd7f51e72886c
Mike <ccteym@gmail.com>
2020-09-17 21:00:25 +0800

    ui

---
+++ app/src/components/navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="navbar">
+    <div :style="{height: statusBarHeight}"></div>
+    <div class="content" :style="{height: navbarHeight}">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      statusBarHeight: 0,
+      navbarHeight: 0
+    }
+  },
+  created () {
+    const {statusBarHeight, platform} = mpvue.getSystemInfoSync()
+    console.log(statusBarHeight, platform)
+    this.statusBarHeight = statusBarHeight + 'px'
+    this.navbarHeight = (platform === 'Android' ? 48 : 44) + 'px'
+  }
+}
+</script>
+
+<style scoped>
+.navbar{
+  width: 100%;
+}
+.content{
+  width: 100%;
+}
+</style>
---
+++ app/src/pages/index/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <navbar>
+      <div class="navbar"></div>
+    </navbar>
+    <div class="content">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import navbar from '@/components/navbar'
+
+export default {
+  data () {
+    return {
+    }
+  },
+
+  components: {
+    navbar
+  },
+  created () {
+    // let app = getApp()
+  },
+  onPullDownRefresh () {
+
+  },
+  onReachBottom () {
+
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+.navbar{
+  background-color: bisque;
+  width: 100%;
+  height: 100%;
+}
+.content{
+  flex: 1;
+  background-color: brown;
+}
+</style>
