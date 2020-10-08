438f889f3c9a2959d8811033d31f423d5d9467bc
mike <ccteym@gmail.com>
2020-10-08 18:14:21 +0800

    ui

--- a_admin/src/api/accounts.js
+++ a_admin/src/api/accounts.js
@@ -2,7 +2,27 @@ import request from '@/utils/request'
 
 export function fetchBloggerList(query) {
   return request({
-    url: '/sys/blogger/list',
+    url: '/sys/account/bl/list',
+    method: 'get',
+    params: query
+  })
+}
+export function fetchBlogger(id) {
+  return request({
+    url: '/sys/account/bl/' + id,
+    method: 'get'
+  })
+}
+export function unbindBlogger(data) {
+  return request({
+    url: '/sys/account/bl/channel/unbind',
+    method: 'post',
+    data
+  })
+}
+export function fetchBloggerFinance(query) {
+  return request({
+    url: '/sys/account/bl/finance/record/list',
     method: 'get',
     params: query
   })
--- a_admin/src/views/accounts/blogger.vue
+++ a_admin/src/views/accounts/blogger.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
+      <el-select v-model="listQuery.searchType" size="mini" style="width: 90px" class="filter-item">
+        <el-option :value="1" label="账号ID" />
+        <el-option :value="2" label="用户昵称" />
+        <!-- <el-option :value="3" label="渠道链接" /> -->
+      </el-select>
       <el-input
-        v-model="listQuery.name"
-        placeholder="请输入博主名称"
+        v-model="listQuery.searchKey"
+        placeholder="请输入"
         style="width: 200px; margin-left: 16px"
         size="mini"
         class="filter-item"
@@ -13,30 +18,22 @@
         v-model="listQuery.platformId"
         size="mini"
         class="filter-item"
-        style="margin-left: 16px"
+        style="width: 90px;margin-left: 16px"
       >
         <el-option :value="0" label="全部" />
-        <el-option
-          v-for="(item, i) in channels"
-          :key="i"
-          :value="item.id"
-          :label="item.name"
-        />
+        <el-option v-for="(item, i) in channels" :key="i" :value="item.id" :label="item.name" />
       </el-select>
 
-      <el-select
-        v-model="listQuery.statusCode"
+      <el-date-picker
+        v-model="listQuery.timeRange"
         size="mini"
         class="filter-item"
         style="margin-left: 16px"
-      >
-        <el-option
-          v-for="(item, i) in status"
-          :key="i"
-          :value="i"
-          :label="item"
-        />
-      </el-select>
+        type="daterange"
+        range-separator="至"
+        start-placeholder="开始日期"
+        end-placeholder="结束日期"
+      />
 
       <el-button
         class="filter-item"
@@ -56,61 +53,42 @@
       highlight-current-row
       style="width: 100%"
     >
-      <el-table-column label="用户ID">
+      <el-table-column label="用户ID" width="90">
         <template slot-scope="{ row }">
           <span>{{ row.id }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="用户昵称" align="center">
+      <el-table-column label="用户昵称" align="center" width="120">
         <template slot-scope="{ row }">
-          <span>{{ row.account.nickname }}</span>
+          <span>{{ row.nickname }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="申请渠道" align="center">
+      <el-table-column label="认证渠道/粉丝数" align="center">
         <template slot-scope="{ row }">
-          <span>{{ row.platformName }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="主页链接" align="center">
-        <template slot-scope="{ row }">
-          <a :href="row.homeLink" target="_blank">{{ row.homeLink }}</a>
-        </template>
-      </el-table-column>
-      <el-table-column label="主页截图" align="center">
-        <template slot-scope="{ row }">
-          <p @click="handlePreview(row.homePic)">查看</p>
-        </template>
-      </el-table-column>
-      <el-table-column label="状态" align="center">
-        <template slot-scope="{ row }">
-          <span>{{ status[row.statusCode] }}</span>
-          <span v-if="row.statusCode === 4"><br>{{ row.rejectReason }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="提交时间" align="center">
-        <template slot-scope="{ row }">
-          <span>{{ row.gmtCreate }}</span>
+          <div v-if="row.channels && row.channels.length > 0">
+            <span v-for="(i, j) in row.channels" :key="j">
+              {{ i.platformName }}/{{ i.fansCount }}
+              <br>
+            </span>
+          </div>
+          <span v-else>未认证</span>
         </template>
       </el-table-column>
 
-      <el-table-column
-        label="操作"
-        align="center"
-        width="240"
-        class-name="small-padding fixed-width"
-      >
+      <el-table-column label="钱包收入" align="center">
         <template slot-scope="{ row }">
-          <el-button
-            v-if="row.statusCode === 1"
-            type="primary"
-            size="mini"
-            @click="handleAction(row, 3)"
-          >通过</el-button>
-          <el-button
-            v-if="row.statusCode === 1"
-            size="mini"
-            @click="handleAction(row, 4)"
-          >拒绝</el-button>
+          <span>{{ row.financeInfo.totalAmount }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="注册时间" align="center">
+        <template slot-scope="{ row }">
+          <span>{{ row.date }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column label="操作" align="center">
+        <template slot-scope="{ row }">
+          <el-button size="mini" @click="handleDetail(row)">详情</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -122,18 +100,133 @@
       :limit.sync="listQuery.size"
       @pagination="getList"
     />
-    <el-dialog width="60%" title="预览" :visible.sync="preview" append-to-body>
-      <img style="width: 100%" :src="previewUrl" alt="img">
+    <el-dialog width="60%" title="达人详情" :visible.sync="detailVisable" append-to-body>
+      <div v-if="detail" class="detail">
+        <div class="row">
+          <h4>账户ID:</h4>
+          <p>{{ detail.id }}</p>
+        </div>
+        <div class="row">
+          <h4>微信昵称:</h4>
+          <p>{{ detail.nickname }}</p>
+        </div>
+        <div class="row">
+          <h4>真实姓名:</h4>
+          <p>{{ detail.realName }}</p>
+        </div>
+        <div class="row">
+          <h4>擅长领域:</h4>
+          <p>{{ detail.areas.map(i=>i.name).join('/') }}</p>
+        </div>
+        <div class="row">
+          <h4>注册时间:</h4>
+          <p>{{ detail.date }}</p>
+        </div>
+        <div class="row">
+          <h4>手机号:</h4>
+          <p>{{ detail.telephone }}</p>
+        </div>
+        <div class="row">
+          <h4>微信号:</h4>
+          <p>{{ detail.wechatNo }}</p>
+        </div>
+        <div class="row">
+          <h4>钱包收入:</h4>
+          <div class="row">
+            <p>{{ detail.id }}</p>
+            <el-button size="mini" @click="handleFinance">明细</el-button>
+          </div>
+        </div>
+        <h4>渠道</h4>
+        <el-table :data="detail.channels" border fit>
+          <el-table-column label="认证渠道">
+            <template slot-scope="{ row }">
+              <span>{{ row.platformName }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="昵称">
+            <template slot-scope="{ row }">
+              <span>{{ row.account }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="粉丝数">
+            <template slot-scope="{ row }">
+              <span>{{ row.fansCount }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="渠道ID">
+            <template slot-scope="{ row }">
+              <span>{{ row.platformUid }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="主页链接">
+            <template slot-scope="{ row }">
+              <a :href="row.homeLink" target="_blank">{{ row.homeLink }}</a>
+            </template>
+          </el-table-column>
+          <el-table-column label="主页截图" align="center">
+            <template slot-scope="{ row }">
+              <el-button size="mini" @click="onPreview(row.homePic)">查看</el-button>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作" align="center">
+            <template slot-scope="{ row }">
+              <el-button size="mini" @click="onUnbind(row)">解绑</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <el-dialog width="60%" title="预览" :visible.sync="previewVisable" append-to-body>
+        <img style="width: 100%" :src="preview" alt="img">
+      </el-dialog>
+      <el-dialog width="60%" title="钱包明细" :visible.sync="financeVisable" append-to-body>
+        <el-table v-loading="finance.loading" :data="finance.list" border fit style="width: 100%">
+          <el-table-column label="类型" width="90">
+            <template slot-scope="{ row }">
+              <span>{{ row.title }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="描述" align="center">
+            <template slot-scope="{ row }">
+              <span>{{ row.msg }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="时间" align="center">
+            <template slot-scope="{ row }">
+              <span>{{ row.date }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="金额" align="center">
+            <template slot-scope="{ row }">
+              <span>{{ row.raeType === 1 ? '+' : '-' }}{{ row.amount }}</span>
+            </template>
+          </el-table-column>
+        </el-table>
+
+        <pagination
+          v-show="finance.total > 0"
+          :total="finance.total"
+          :page.sync="finance.page"
+          :limit.sync="finance.size"
+          @pagination="loadFinance"
+        />
+      </el-dialog>
     </el-dialog>
   </div>
 </template>
 
 <script>
-import { fetchBloggerList } from '@/api/accounts'
+import {
+  fetchBloggerList,
+  fetchBlogger,
+  unbindBlogger,
+  fetchBloggerFinance
+} from '@/api/accounts'
 import { clearQueryObject } from '@/utils/index'
 import { Channels } from '@/utils/constant'
+import moment from 'moment'
 import waves from '@/directive/waves' // waves directive
-import { mapGetters } from 'vuex'
+// import { mapGetters } from "vuex";
 import Pagination from '@/components/Pagination' // secondary package based on el-pagination
 
 export default {
@@ -149,70 +242,128 @@ export default {
       listQuery: {
         page: 1,
         size: 20,
-        name: undefined,
-        statusCode: 0,
-        platformId: 0
+        searchType: 1,
+        searchKey: '',
+        platformId: 0,
+        timeRange: []
       },
       channels: Channels,
-      status: ['全部', '未认证', '审核中', '已认证', '已拒绝'],
-      preview: false,
-      previewUrl: ''
+      detailVisable: false,
+      detail: null,
+      previewVisable: false,
+      preview: '',
+      financeVisable: false,
+      finance: {
+        loading: false,
+        list: [],
+        page: 1,
+        size: 5,
+        total: 0
+      }
     }
   },
-  computed: {
-    ...mapGetters(['name', 'avatar', 'telephone'])
-  },
+  // computed: {
+  //   ...mapGetters(["name", "avatar", "telephone"]),
+  // },
   created() {
     this.getList()
   },
   methods: {
     getList() {
       this.listLoading = true
-      fetchBloggerList(clearQueryObject(this.listQuery, true)).then(
-        ({ data }) => {
-          this.list = data.data
-          this.total = data.pager.count
+      const obj = Object.assign({}, this.listQuery)
+      obj[['key', 'accountId', 'nickname'][this.listQuery.searchType]] =
+        obj.searchKey
+      if (obj.timeRange.length > 0) {
+        obj.startTime = moment(this.listQuery.timeRange[0]).format(
+          'YYYY-MM-DD HH:mm:ss'
+        )
+        obj.endTime = moment(this.listQuery.timeRange[0]).format(
+          'YYYY-MM-DD HH:mm:ss'
+        )
+        obj.timeRange = ''
+      }
+      fetchBloggerList(clearQueryObject(obj, true)).then(({ data }) => {
+        this.list = data.data.map((i) =>
+          Object.assign(i, {
+            date: moment(i.gmtCreate).format('YYYY-MM-DD HH:mm:ss')
+          })
+        )
+        this.total = data.pager.count
 
-          // Just to simulate the time of the request
-          setTimeout(() => {
-            this.listLoading = false
-          }, 1.5 * 1000)
-        }
-      )
+        // Just to simulate the time of the request
+        setTimeout(() => {
+          this.listLoading = false
+        }, 1.5 * 1000)
+      })
+    },
+    loadDetail(item) {
+      fetchBlogger(item.id).then((r) => {
+        r.data.financeInfo = item.financeInfo
+        r.data.channels = item.channels
+        r.data.date = item.date
+        this.detail = r.data
+      })
+    },
+    loadFinance() {
+      this.finance.loading = true
+      fetchBloggerFinance({
+        page: this.finance.page,
+        size: this.finance.size,
+        blAccountId: this.detail.id
+      })
+        .then((r) => {
+          this.finance.list = r.data.data.map((i) =>
+            Object.assign(i, {
+              date: moment(i.gmtCreate).format('YYYY-MM-DD HH:mm:ss')
+            })
+          )
+          this.finance.total = r.data.pager.count
+          this.finance.loading = false
+        })
+        .catch((e) => {
+          this.finance.loading = false
+        })
     },
     handleFilter() {
       this.listQuery.page = 1
       this.getList()
     },
-
-    handlePreview(url) {
-      this.previewUrl = url
-      this.preview = true
-    },
-    // handleAction(row, state) {
-    //   if (state === 3) {
-    //     this.$confirm('确认通过?').then((r) => {
-    //       updateBloggerState({ id: row.id, statusCode: state }).then((r) => {
-    //         row.statusCode = state
-    //         this.$message({ message: '操作成功', type: 'success' })
-    //       })
-    //     })
-    //   } else {
-    //     this.$prompt('请输入拒绝理由').then((r) => {
-    //       updateBloggerState({
-    //         id: row.id,
-    //         statusCode: state,
-    //         rejectReason: r.value
-    //       }).then(() => {
-    //         row.statusCode = state
-    //         row.rejectReason = r.value
-    //         this.$message({ message: '操作成功', type: 'success' })
-    //       })
-    //     })
-    //   }
-    // },
     handleDetail(item) {
-      console.log(item.name)
+      this.loadDetail(item)
+      this.detailVisable = true
+    },
+    onPreview(url) {
+      this.preview = url
+      this.previewVisable = true
+    },
+    onUnbind(row) {
+      this.$confirm(
+        `确定解绑${this.detail.nickname}的${row.platformName}渠道，解绑后达人需重新提交渠道认证申请`
+      ).then((r) => {
+        unbindBlogger({
+          blAccountId: this.detail.id,
+          platformId: row.platformId
+        }).then((r) => {
+          const index = this.detail.channels.findIndex(
+            (i) => i.platformId === row.platformId
+          )
+          if (index >= 0) {
+            this.detail.channels.splice(index, 1)
+          }
+        })
+      })
+    },
+    handleFinance() {
+      this.finance = {
+        loading: false,
+        list: [],
+        page: 1,
+        size: 5,
+        total: 0
+      }
+      this.financeVisable = true
+      this.loadFinance()
     }
   }
 }
@@ -248,6 +399,39 @@ export default {
     vertical-align: middle;
     margin: 0;
     padding: 0;
+  }
+}
+.detail {
+  .row {
+    display: flex;
+    flex-direction: row;
+    margin-top: 4px;
+    h4 {
+      width: 80px;
+      margin: 0;
+      padding: 0;
+      line-height: 28px;
+    }
+    h6 {
+      width: 80px;
+      margin: 0;
+      padding: 0;
+      line-height: 28px;
+    }
+    p {
+      margin: 0;
+      padding: 0;
+      line-height: 28px;
+    }
+    .el-radio {
+      margin-top: 6px;
+    }
+    .el-input {
+      width: 280px;
+    }
+    div {
+      flex: 1;
+    }
   }
 }
 </style>
